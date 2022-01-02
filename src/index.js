4713a399e516502138a80cd33d0e6056850e37b9
Man10j <cmprabhakarjan27@gmail.com>
2022-01-02 19:29:15 +0530

    config firebase

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers/rootReducer";
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+const firebaseConfig = {
+  apiKey: "AIzaSyDYOqVQRLZpRbGBmpvO27-CVHMtiQsgeWQ",
+  authDomain: "moviesearchreact.firebaseapp.com",
+  projectId: "moviesearchreact",
+  storageBucket: "moviesearchreact.appspot.com",
+  messagingSenderId: "66018623313",
+  appId: "1:66018623313:web:d78f9da31de4146c7fdd70",
+  measurementId: "G-ZXXQPVRW40",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const store = createStore(rootReducer);
 
 ReactDOM.render(
